ecc4a31de53bcec1ffc70d79d3f6d3e3572b8cd2
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2023-04-04 19:55:25 +0600

    complete Array section

--- Section 2/script.js
+++ Section 2/script.js
@@ -3,29 +3,29 @@
 // const private ="joy";
 // console.log(private);
 
-function car()
-{
-    console.log("Car has a wheel");
-}
-car();
+// function car()
+// {
+//     console.log("Car has a wheel");
+// }
+// car();
 
-function food(applle,mango)
-{
-    console.log(applle,mango);
-    const juce =`${applle} is a ${mango}`;
-    return juce;
-}
-console.log(food(2,3));
+// function food(applle,mango)
+// {
+//     console.log(applle,mango);
+//     const juce =`${applle} is a ${mango}`;
+//     return juce;
+// }
+// console.log(food(2,3));
 
-const value = function(number)
-{
-   const nm= `the numhber is ${number}`;
-   return nm;
+// const value = function(number)
+// {
+//    const nm= `the numhber is ${number}`;
+//    return nm;
 
-}
+// }
 
-const nn= value(23);
-console.log(nn);
+// const nn= value(23);
+// console.log(nn);
 
 // const information = (bio,bio1 )=>
 // {
@@ -38,11 +38,46 @@ console.log(nn);
 //information(2,3);
 
 
-function factorial(n)
+// function factorial(n)
+// {
+//     if(n > 1)
+//     return n*factorial(n - 1);
+//   else
+//     return 1;
+// }
+// console.log(factorial(5));
+
+
+//array
+ 
+const info =['joy', 'matubber ',' rana',23,33.44];
+console.log(info.length);
+console.log(info[3]);
+
+// add elemtn in array 
+
+info.push("tusar");  // add last 
+console.log(info);
+info.unshift("lisun");   // add first 
+console.log(info);
+// delete element 
+info.pop();
+console.log(info);
+info.shift();
+console.log(info);
+
+console.log(info.includes("joy"));
+console.log(info.includes("sumaiya "));
+
+let n=5;
+const checkmax=[1,6,3,8,4,7];
+let ma=checkmax[0];
+for(let i=0;i<checkmax.length;i++)
 {
-    if(n > 1)
-    return n*factorial(n - 1);
-  else
-    return 1;
+    if(checkmax[i]>ma)
+    {
+        ma=checkmax[i];
+
+    } 
 }
-console.log(factorial(5));
+console.log(ma);
