f361df926212dc09cf5f1d21686a614451b5696b
JoyMatubber <joy.matubber@Brainstation-23.com>
2023-04-05 03:26:57 +0600

    section 3 added and complete lecture 008

---
+++ Section 3/script.js
@@ -0,0 +1,21 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+// download pritter
+// file -> prefarence->settinf->default for esb
+// formatons -enable
+
+// todo highlite
+
+// devoloper mozila .org
+//MDN web Docs
+
+const x = 23;
+if (x == 1) console.log("oj");
+console.log("gdgfdgfgf");
+console.log();
+
+
+const valu=prompt("enter a value");
+const ans=valu+10;
+console.table(valu);
+console.log(ans);
